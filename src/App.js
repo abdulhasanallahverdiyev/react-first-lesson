1994da8cb9a724caef834956a5abae4d7529c560
abdulhasanallahverdiyev <abdulhsn.allahverdiyev@mail.ru>
2023-02-14 06:20:33 +0400

    Environment Variables

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      ABDULhasan
+   
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>{process.env.REACT_APP_API_URL}</p>
+      {
+        process.env.NODE_ENV === 'production' && (
+          <div>
+            <h1>Salam Menim adim Abdulhasan di</h1>
+          </div>
+        )
+      }
     </div>
   );
 }
